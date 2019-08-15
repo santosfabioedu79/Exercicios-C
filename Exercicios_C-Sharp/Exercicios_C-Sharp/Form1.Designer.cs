c353113b005b3b0528d158a154efc8f57ff3b4f6
Fábio Eduardo dos Santos <fabio.santos>
2019-08-15 19:57:28 -0300

    Exercicio 1

---
+++ Exercicios_C-Sharp/Exercicios_C-Sharp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Exercicios_C_Sharp
+{
+    partial class fmrExercicio1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe um número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(118, 49);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(127, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(18, 93);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // fmrExercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "fmrExercicio1";
+            this.Text = "Exercicio 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Exercicios_C-Sharp/Exercicios_C-Sharp/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicios_C_Sharp
+{
+    public partial class fmrExercicio1 : Form
+    {
+        
+
+        public fmrExercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            var numero = Convert.ToInt32(txtNumero.Text);
+
+            if (numero % 2 == 0)
+            {
+                MessageBox.Show($"O {numero} é número primo");
+            }
+            else
+            {
+                MessageBox.Show($"O {numero} não é número primo");
+            }
+        }
+    }
+}
